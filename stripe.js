5f3e08a59227f91ad1eca1086fedc2553cc22cda
Justin Wilson <justin@jwils.co>
2019-10-13 17:00:00 -0400

    second commit

---
+++ stripe.js
@@ -0,0 +1,46 @@
+window.Stripe = () => {
+  return {
+    elements: () => {
+      return {}
+    },
+    createToken: () => {
+      return {}
+    },
+    createSource: () => {
+      return {}
+    },
+    createPaymentMethod: () => {
+      return {}
+    },
+    retrieveSource: () => {
+      return {}
+    },
+    paymentRequest: () => {
+      return {}
+    },
+    redirectToCheckout: () => {
+      return {}
+    },
+    retrievePaymentIntent: () => {
+      return {}
+    },
+    handleCardPayment: () => {
+      return {}
+    },
+    handleCardAction: () => {
+      return {}
+    },
+    confirmPaymentIntent: () => {
+      return {}
+    },
+    handleCardSetup: () => {
+      return {}
+    },
+    retrieveSetupIntent: () => {
+      return {}
+    },
+    confirmSetupIntent: () => {
+      return {}
+    }
+  }
+}
